04df15df947443322806907ebc7f94ca99d20385
charu07goel <charu07goel@gmail.com>
2018-04-07 17:25:30 +0530

    committed

--- app/src/main/java/com/example/charugoel/readydoc/First.java
+++ app/src/main/java/com/example/charugoel/readydoc/First.java
@@ -6,11 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class First extends AppCompatActivity {
-    public Button but1,but2;
+    public ImageButton but1,but2;
     public void init(){
-        but1 = (Button)findViewById(R.id.button2);
+        but1 = (ImageButton) findViewById(R.id.button2);
         but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -18,7 +19,7 @@ public class First extends AppCompatActivity {
                 startActivity(toy);
             }
         });
-        but2 = (Button)findViewById(R.id.button);
+        but2 = (ImageButton)findViewById(R.id.button);
         but2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/charugoel/readydoc/SplashScreen.java
+++ app/src/main/java/com/example/charugoel/readydoc/SplashScreen.java
@@ -14,7 +14,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(2000);
                     Intent intent = new Intent(getApplicationContext(), First.class);
                     startActivity(intent);
                     finish();
